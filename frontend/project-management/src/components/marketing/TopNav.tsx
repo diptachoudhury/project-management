edd70e227784b0e817a2badac3e9554ff9881934
diptachoudhury <diptachoudhury53@gmail.com>
2025-06-15 17:03:13 +0530

    diable dark mode

--- frontend/project-management/src/components/marketing/TopNav.tsx
+++ frontend/project-management/src/components/marketing/TopNav.tsx
@@ -69,7 +69,7 @@ export default function TopNav() {
       <div className={`max-w-screen-lg  shadow-xl flex flex-wrap items-center justify-between mx-auto p-3 px-8 ${!scrolled ? 'bg-white':'bg-white bg-opacity-90' } rounded-full`}>
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://placehold.co/32x32/6366f1/ffffff?text=TB" className="h-7 rounded-full" alt="TB Logo" />
-          <span className="self-center text-sm font-semibold whitespace-nowrap text-gray-900 dark:text-white">
+          <span className="self-center text-sm font-semibold whitespace-nowrap text-gray-900 ">
             Ticket Bucket
           </span>
         </Link>
@@ -95,7 +95,7 @@ export default function TopNav() {
           {/* Mobile Menu Button (Hamburger Icon) */}
           <button
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 transition-colors duration-200"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  transition-colors duration-200"
             aria-controls="navbar-sticky"
             aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
             onClick={toggleMobileMenu} 
@@ -114,11 +114,11 @@ export default function TopNav() {
           }`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 text-sm font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 text-sm font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
             <li>
               <Link
                 href={`/tickets/${domainName}`}
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0"
                 aria-current="page"
               >
                 Dashboard
@@ -127,7 +127,7 @@ export default function TopNav() {
             <li>
               <Link
                 href="/tasks"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors duration-200"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  transition-colors duration-200"
               >
                 My-Tasks
               </Link>
@@ -135,7 +135,7 @@ export default function TopNav() {
             <li>
               <Link
                 href="/members"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors duration-200"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 transition-colors duration-200"
               >
                 Org-Members
               </Link>
@@ -143,7 +143,7 @@ export default function TopNav() {
             <li>
               <Link
                 href={`/tickets/${domainName}`}
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition-colors duration-200"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  transition-colors duration-200"
               >
                 Create a Ticket
               </Link>
